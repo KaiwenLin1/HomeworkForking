184dfdb59fd1206808829742ad8e97a0722e1ff1
lkw16398001 <78048928+lkw16398001@users.noreply.github.com>
2023-04-05 23:21:37 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,9 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+
+diamonds1 <- as.data.table(diamonds)
+diamonds1[cut=="Premium" & price>7000 & price<10000, list(carat,cut,price)] -> diamonds1
+diamonds1[order(-carat)] ->diamonds1
+diamonds1[1:20]
